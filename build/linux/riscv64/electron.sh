c4c49c593ed97d4b4c075517900080e340a19928
wadkisson <adkisson@wustl.edu>
2025-06-07 20:40:05 -0500

    Initial commit: clean state of Lean4Code

---
+++ build/linux/riscv64/electron.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -ex
+
+export ELECTRON_VERSION="34.5.4"
+export VSCODE_ELECTRON_TAG="v${ELECTRON_VERSION}.riscv1"
---
+++ vscode/extensions/terminal-suggest/src/completions/codium-insiders.ts
@@ -0,0 +1,9 @@
+import code from './code';
+
+const codiumInsidersCompletionSpec: Fig.Spec = {
+	...code,
+	name: 'codium-insiders',
+	description: 'VSCodium Insiders',
+};
+
+export default codiumInsidersCompletionSpec;
